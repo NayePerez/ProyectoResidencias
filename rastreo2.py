576bd492e51f266c1d4d3389ebb3d7f127a4360c
NayePerez <nayely_perezleyva@hotmail.com>
2022-05-31 12:36:59 -0500

    Primera prueba seccion1

--- rastreo2.py
+++ rastreo2.py
@@ -12,18 +12,19 @@ class linea():
 def reporteError(mensaje):
     print(mensaje)   
     
-#Verifica si la temperatura de un punto adyacente esta fuera de rango
+#Verifica si la temperatura de un punto cercano esta fuera de rango
 def verificarTemperatura(temperatura : float):
-    if  temperatura > 26 or  temperatura < 22:
+    if  temperatura > 26.0 or  temperatura < 22.0:
         print("La temperatura de esta linea tambien esta fuera de rango" + str(temperatura))
         return temperatura
     return 0
-#Calcula el promedio de los 3 puntos adyacentes fuera de rango
+
+#Calcula el promedio de los puntos cercanos fuera de rango
 def calcularPromedio(listapc):
     suma = 0
     for pc in listapc:
         suma = suma + int(pc)
-    return suma / len(listapc)
+    return suma / (len(listapc) or 1) #para evitar la division entre 0
 
 
 
@@ -39,14 +40,13 @@ try:
     ruta = os.path.abspath(ruta)+'\\Resultados' #Ruta win
 except:
     reporteError('Error al obtener la ruta')
-
-
+    
 lineas = [] #Lista de lineas
 listaUbicaciones = [] #lista ubicaciones de cada punto en la línea
-listaTemperaturas = [] #lista temperaturas de cada punto en la línea
+listaTemperaturas = [] #lista temperaturas de cada punto en la línea 
 
 #Leerá los 36 archivos de la carpeta resultados y guardará la informacion necesaria en la lista lineas 
-for x in range(1,2):
+for x in range(1,13):
     nombreArchivoLinea = str(x) + '.txt' 
     #Lee el archivo y guarda su informacion completa en la variable data
     try:
@@ -77,23 +77,24 @@ for l in lineas:
     for x in range(len(l.ubicacion)):        
         print(str(l.ubicacion[x]) + "-----" + str(l.temperatura[x]) ) 
     
-k = 0 # k: índice del punto analizado.
+i = 0 # k: índice del punto analizado.
 pc =  False  # pc: punto crítico boolean
-npc = 0 # npc: número de punto crítico 
 tp = 0 # tp: temperatura promedio
 tpm = 0 # tpm: temperatura promedio máxima 
 listapc = [] # pc1, pc2, pc3: punto crítico 1, 2 y 3. (temperatura)
-punto = 0 
+punto = 0  #es la ubicacion central de la zona termica fuera de rango
 
 #Buscar la ubicacion de la zona de calor de la seccion 1[1-12]
 for x in range(1,13):
     for t in lineas[x].temperatura:
-        i = t.index()
+        
+        #se leen los datos de temperatura de cada punto de la línea en busca de una temperatura fuera de rango
         if verificarTemperatura(t) != 0:
             pc = True
-            npc = npc + 1
+            i = t.index()
             listapc.append(t)
-            #!!!!!!!!Revisar lo que va  a pasar cuando estes en el ultimo punto de la lista, porque ya no habra dos siguientes puntos 
+            # Cuando se analice penultimo y ultimo punto de punto de la lista esto podria ocasionar un nullpointer ex
+            #Debido a esto se analiza hasta el antepenultimo punto
             if i <= (len(lineas[x].temperatura)-2):
                 if verificarTemperatura(lineas[x].temperatura[i+1]) != 0:
                     npc = npc +1
@@ -101,6 +102,7 @@ for x in range(1,13):
                 if verificarTemperatura(lineas[x].temperatura[i+2]) != 0:
                     npc = npc +1
                     listapc.append(t)
+                    
             #11.Esquina inferior izquierda: 1; arriba, derecha. (+1, +3)
             if x == 1:
                 if verificarTemperatura(lineas[x+1].temperatura[i]) != 0:
@@ -169,7 +171,6 @@ for x in range(1,13):
             punto = lineas[x].ubicacion[i]
             tpm = tp
         pc = False
-        npc = 0
         listapc.clear()
         
             
@@ -179,13 +180,13 @@ for x in range(1,13):
     
     
 #Buscar la ubicacion de la zona de calor de la seccion 2[13-24]
-for x in range(13,25):
+''' for x in range(13,25):
     if x == 1:
         print("h")
     elif x == 2:
         print("h")
     elif x == 3:
-        print("h")
+        print("h") '''
     #3.	Superior: 6, 9, 12, 15, 18, 21; abajo, izquierda, derecha. (-1, -3, +3)
     #1.	Centro: 5, 8, 11, 14, 17, 20; arriba, abajo, izquierda, derecha. (+1,-1,-3, +3)
     #7.	Inferior: 4, 7, 10, 13, 16, 19; arriba, izquierda, derecha. (+1,-3, +3)
@@ -195,25 +196,24 @@ for x in range(13,25):
     
     
 #Buscar la ubicacion de la zona de calor de la seccion 3[25-32]
-for x in range(25,33):
+''' for x in range(25,33):
     if x == 1:
         print("h")
     elif x == 2:
         print("h")
-    elif x == 3:
-        print("h")
+        print("h") '''
     #5.	Superior-26, 28, 30, 32; abajo, izquierda, derecha. (-1, -2, +2)
     #8.	Inferior-25, 27, 29; arriba, izquierda, derecha. (+1, -2, +3)
     #13.Esquina inferior derecha-31; arriba, izquierda. (+1,-2)
     
 #Buscar la ubicacion de la zona de calor de la seccion 4[33-36]
-for x in range(33,37):
+''' for x in range(33,37):
     if x == 1:
         print("h")
     elif x == 2:
         print("h")
     elif x == 3:
-        print("h")
+        print("h") '''
     #6.	Lineal: 33, 34, 35; izquierda, derecha. (-1, +1)
     #14.	Esquina lateral derecha: 36; izquierda. (-1)
 
